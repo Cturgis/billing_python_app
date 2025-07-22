a19bf0f2e417b7fe5c5f4e4e22db6f0cb0034b89
Corentin TURGIS <corentin.turgis@stayopen.io>
2025-07-22 13:31:08 +0200

    Add styles

--- projects/billing/forms/customer_form.py
+++ projects/billing/forms/customer_form.py
@@ -1,11 +1,12 @@
 from django import forms
 from billing.models.Customer import Customer
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 class CustomerForm(forms.ModelForm):
     username = forms.CharField(max_length=150, label='Nom d\'utilisateur')
     email = forms.EmailField(label='Email')
     password = forms.CharField(widget=forms.PasswordInput, label='Mot de passe')
+    is_admin = forms.BooleanField(label="Utilisateur admin", required=False)
 
     class Meta:
         model = Customer
@@ -15,11 +16,24 @@ class CustomerForm(forms.ModelForm):
         }
 
     def save(self, commit=True):
-        user = User.objects.create_user(
-            username=self.cleaned_data['username'],
-            email=self.cleaned_data['email'],
-            password=self.cleaned_data['password']
-        )
+        # If updating, get the existing user
+        if self.instance and self.instance.pk:
+            user = self.instance.user
+            user.email = self.cleaned_data['email']
+            if self.cleaned_data['password']:
+                user.set_password(self.cleaned_data['password'])
+            user.save()
+        else:
+            user = User.objects.create_user(
+                username=self.cleaned_data['username'],
+                email=self.cleaned_data['email'],
+                password=self.cleaned_data['password']
+            )
+            if self.cleaned_data.get('is_admin'):
+                group, _ = Group.objects.get_or_create(name='admin')
+            else:
+                group, _ = Group.objects.get_or_create(name='customer')
+            user.groups.add(group)
         customer = super().save(commit=False)
         customer.user = user
         if commit:
